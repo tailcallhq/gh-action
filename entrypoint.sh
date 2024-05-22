1332118304297c2d90a082f346ec5d40ea092356
Shashi Kant <shashitnak@gmail.com>
2024-05-22 15:18:31 +0000

    taking config contents

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export TF_VAR_AWS_REGION=$AWS_REGION
 export TF_VAR_AWS_IAM_ROLE=$AWS_IAM_ROLE
 export TF_VAR_AWS_LAMBDA_FUNCTION_NAME=$AWS_LAMBDA_FUNCTION_NAME
 export TF_VAR_TAILCALL_VERSION=$TAILCALL_VERSION
-mv $TAILCALL_CONFIG /tmp/config.graphql
+echo $TAILCALL_CONFIG > /tmp/config.graphql
 
 cd /tmp
 terraform init
