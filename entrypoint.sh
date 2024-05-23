67c09d3f9619e5b410f6e6230f8c61cba2f2e3c8
Shashi Kant <shashitnak@gmail.com>
2024-05-23 22:42:50 +0000

    download terraform binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-get_latest_release() {
+get_latest_version() {
   curl https://api.github.com/repos/$1/$2/releases/latest -s | jq .name -r
 }
 
@@ -11,12 +11,19 @@ export TF_VAR_AWS_IAM_ROLE=$AWS_IAM_ROLE
 export TF_VAR_AWS_LAMBDA_FUNCTION_NAME=$AWS_LAMBDA_FUNCTION_NAME
 
 if [ "$TAILCALL_VERSION" = "latest" ]; then
-  TAILCALL_VERSION=$(get_latest_release tailcallhq tailcall)
+  TAILCALL_VERSION=$(get_latest_version tailcallhq tailcall)
 fi
 export TF_VAR_TAILCALL_VERSION=$TAILCALL_VERSION
 
 get_tailcall_config() {
   cp "$TAILCALL_CONFIG" .
+}
+
+setup_terraform() {
+  TERRAFORM_VERSION=$(get_latest_version hashicorp terraform)
+  wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  unzip /tmp/terraform.zip -d /tmp && rm /tmp/terraform.zip
+  mv /tmp/*/terraform /usr/local/bin/terraform
 }
 
 setup_flyctl() {
