aa14132af2d6028c4fa46d6ca5d996f630fb51d9
Shashi Kant <shashitnak@gmail.com>
2024-06-11 12:36:20 +0530

    adding newlines

--- scripts/create-tf-zip.sh
+++ scripts/create-tf-zip.sh
@@ -6,7 +6,7 @@ depends_on="[";
 for file_path in $(find . -type f); do
   echo "Path: $file_path"
   name=$(basename $file_path | tr '.' '_')
-  resource="resource \"local_sensitive_file\" \"$name\" { content_base64 = filebase64(\"$file_path\") filename = \"config\" }"
+  resource="resource \"local_sensitive_file\" \"$name\" {\n content_base64 = filebase64(\"$file_path\") \n filename = \"config\"\n }"
 
   printf "\n$resource\n" >> /aws/tailcall.tf
 
@@ -15,5 +15,5 @@ for file_path in $(find . -type f); do
 done
 depends_on="$depends_on]"
 
-archive_file="data \"archive_file\" \"tailcall\" { depends_on = $depends_on type = \"zip\" source_dir = \"config\" output_path = \"tailcall.zip\" }"
+archive_file="data \"archive_file\" \"tailcall\" { \n depends_on = $depends_on \n type = \"zip\" \n source_dir = \"config\" \n output_path = \"tailcall.zip\"\n }"
 echo "\n$archive_file\n" >> /aws/tailcall.tf
