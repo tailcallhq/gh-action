aa95a78bf9878d37532ab662d0b13de9c103a547
Shashi Kant <shashitnak@gmail.com>
2024-06-11 13:14:07 +0530

    fixing config path

--- scripts/create-tf-zip.sh
+++ scripts/create-tf-zip.sh
@@ -6,7 +6,9 @@ depends_on="[ local_sensitive_file.bootstrap,";
 for file_path in $(find . -type f); do
   echo "Path: $file_path"
   name=$(basename $file_path | tr '.' '_')
-  resource="resource \"local_sensitive_file\" \"$name\" {\n content_base64 = filebase64(\"$file_path\") \n filename = \"config\"\n }"
+  config_path="${file_path/#./config}"
+
+  resource="resource \"local_sensitive_file\" \"$name\" {\n content_base64 = filebase64(\"$file_path\") \n filename = \"$config_path\"\n }"
 
   printf "\n$resource\n" >> /aws/tailcall.tf
 
