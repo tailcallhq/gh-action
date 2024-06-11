d5191f1bf01d157579e1872aea7d47bf59208218
Shashi Kant <shashitnak@gmail.com>
2024-06-11 12:33:51 +0530

    fixing bug

--- scripts/create-tf-zip.sh
+++ scripts/create-tf-zip.sh
@@ -8,7 +8,7 @@ for file_path in $(find . -type f); do
   name=$(basename $file_path | tr '.' '_')
   resource="resource \"local_sensitive_file\" \"$name\" { content_base64 = filebase64(\"$file_path\") filename = \"config\" }"
 
-  echo "\n$resource" >> /aws/tailcall.tf
+  printf "\n$resource\n" >> /aws/tailcall.tf
 
   depends_on="$depends_on local_sensitive_file.$name,"
   echo $resource
@@ -16,4 +16,4 @@ done
 depends_on="$depends_on]"
 
 archive_file="data \"archive_file\" \"tailcall\" { depends_on = $depends_on type = \"zip\" source_dir = \"config\" output_path = \"tailcall.zip\" }"
-echo "\n$archive_file" >> /aws/tailcall.tf
+echo "\n$archive_file\n" >> /aws/tailcall.tf
