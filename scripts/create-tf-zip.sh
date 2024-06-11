ba86028e4d63f39a683c848c81fcfa3c1af77f3d
Shashi Kant <shashitnak@gmail.com>
2024-06-11 12:53:25 +0530

    fixing bootsrap error

--- scripts/create-tf-zip.sh
+++ scripts/create-tf-zip.sh
@@ -2,7 +2,7 @@
 
 cd /app
 
-depends_on="[";
+depends_on="[ local_sensitive_file.bootstrap,";
 for file_path in $(find . -type f); do
   echo "Path: $file_path"
   name=$(basename $file_path | tr '.' '_')
@@ -13,7 +13,7 @@ for file_path in $(find . -type f); do
   depends_on="$depends_on local_sensitive_file.$name,"
   echo $resource
 done
-depends_on="$depends_on]"
+depends_on="$depends_on ]"
 
 archive_file="data \"archive_file\" \"tailcall\" { \n depends_on = $depends_on \n type = \"zip\" \n source_dir = \"config\" \n output_path = \"tailcall.zip\"\n }"
 printf "\n$archive_file\n" >> /aws/tailcall.tf
